937e98000ed9827d6c2dc66cab3d07f708c849ce
Aman <amanprajapati7266@gmail.com>
2024-02-10 19:02:44 +0530

    add

--- Basic/normal.py
+++ Basic/normal.py
@@ -1,2 +1,7 @@
-name = input("What is your name = ")
-print("Hello"+name)
+# name = input("What is your name = ")
+# print("Hello"+name)
+
+old_age = input("enter your age : ")
+new_age = int(old_age)+2
+print(new_age)
+      
