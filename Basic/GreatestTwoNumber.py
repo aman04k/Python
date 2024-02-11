53592606c0531bfddb749079cfd7ccf7e052aad1
Aman <amanprajapati7266@gmail.com>
2024-02-11 17:26:44 +0530

    add

---
+++ Basic/GreatestTwoNumber.py
@@ -0,0 +1,11 @@
+# num1, num2=20,30
+# if num1>num2:
+#     print(num1)
+# else:
+#     print(num2)    
+
+num1, num2 = 20 , 30
+if num1>num2:
+   print(num1)
+else:
+    print(num2)
---
+++ Basic/LeapYear.py
@@ -0,0 +1,6 @@
+year=4000
+
+if(year%400==0) or (year%4==0 and year %100 != 0):
+    print("This is Leap Year")
+else:
+    print("this is not leap Year")    
