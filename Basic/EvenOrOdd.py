57ae2f800120135a55f783136e1afd8f5d49150f
Aman <amanprajapati7266@gmail.com>
2024-02-11 17:19:24 +0530

    add

---
+++ Basic/EvenOrOdd.py
@@ -0,0 +1,14 @@
+# num = int(input("Enter a number"))
+# if num % 2 ==0:
+#     print("this is even number")
+# else:
+#     print('this is odd number')    
+
+
+#sum of n number
+num=5
+sum=0
+for i in range(num+1):
+    sum += i
+print(sum)    
+
---
+++ Basic/PostiveorNeg.py
@@ -0,0 +1,7 @@
+num =15
+if num > 0:
+    print('postive')
+if num < 0:
+    print('Negative')
+else:
+    print('zero')    
